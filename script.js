380cb67faa118b423f65bb15d8a113d6bf4161de
jamiukayode <jamiuayepekayode@gmail.com>
2025-11-12 11:51:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("contactBtn").addEventListener("click", function () {
+  alert("Thank you for your interest! Please contact us at techsheikh@gmail.com or visit our office.");
+});
