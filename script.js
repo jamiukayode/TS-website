6f72f4571a05d08da7314d314bee027ab4eae4e0
jamiukayode <jamiuayepekayode@gmail.com>
2025-11-14 21:39:52 +0100

    updated about page

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById("contactBtn").addEventListener("click", function () {
-  alert("Thank you for your interest! Please contact us at techsheikh@gmail.com or visit our office.");
+  alert("Thank you for your interest! Please contact us at techsheikh001@gmail.com or visit our office.");
 });
